b55241750aa6e9c0aba75daadb9e2fd3507bfa88
Jon Nordby <jononor@gmail.com>
2025-01-03 17:09:44 +0100

    CI: Try fix install

--- install_unix.sh
+++ install_unix.sh
@@ -3,5 +3,5 @@ MIP_INSTALL="micropython -m mip install"
 
 ${MIP_INSTALL} https://emlearn.github.io/emlearn-micropython/builds/master/x64_6.3/emlearn_trees.mpy
 ${MIP_INSTALL} github:jonnor/micropython-npyfile
-${MIP_INSTALL} https://github.com/emlearn/emlearn-micropython/raw/refs/heads/master/examples/har_trees/recorder.py
-${MIP_INSTALL} https://github.com/emlearn/emlearn-micropython/raw/refs/heads/master/examples/har_trees/timebased.py
+${MIP_INSTALL} https://raw.githubusercontent.com/emlearn/emlearn-micropython/refs/heads/master/examples/har_trees/timebased.py
+${MIP_INSTALL} https://raw.githubusercontent.com/emlearn/emlearn-micropython/refs/heads/master/examples/har_trees/recorder.py
